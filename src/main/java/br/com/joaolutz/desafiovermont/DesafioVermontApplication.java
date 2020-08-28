217256f5fbace8e10d69bdabe7a06d00f99794fe
João Gabriel Messi Lutz <joao.messilutz@gmail.com>
2020-08-28 14:21:01 -0300

    Initial commit

---
+++ src/main/java/br/com/joaolutz/desafiovermont/DesafioVermontApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaolutz.desafiovermont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVermontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVermontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaolutz/desafiovermont/DesafioVermontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaolutz.desafiovermont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVermontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
